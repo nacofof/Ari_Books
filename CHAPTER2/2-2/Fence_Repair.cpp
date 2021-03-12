29d73dc4550e1e13742eca99ea7f554ee1d624b8
nacofof <aiei.numasu@gmail.com>
2021-03-12 14:01:21 +0900

    Fence Repair問題（途中まで）

---
+++ CHAPTER2/2-2/Fence_Repair.cpp
@@ -0,0 +1,18 @@
+//p49~
+#include <iostream>
+
+#define MAX_N 20000
+#define MAX_L 50000
+typedef long long ll;
+
+//入力
+int N = 3, L[MAX_N] = {8, 5, 8};
+
+int main(void){
+    ll ans = 0;
+
+    //板が１枚になるまで適用
+    while (N > 1){
+        //一番短い板mii1，
+    }
+}
